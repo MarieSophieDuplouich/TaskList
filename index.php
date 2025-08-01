f4352f7608b843adb54f9150caebbd70520e7817
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-08-01 14:25:53 +0200

    design amélioré j'aurai fini dimanche

--- index.php
+++ index.php
@@ -66,12 +66,12 @@ $taskss = trouver_tâche_par_userid($_SESSION['user_id']); // c'est ça le read
     </header>
 
     <h2>Bienvenue <?= htmlspecialchars($_SESSION['nom_utilisateur']) ?> sur la page d'administration</h2>
- 
+
     <!-- Formulaire pour ajouter une tâche en front-end -->
     <form action="" method="POST">
         <input type="text" name="name" placeholder="Nom de la tâche" required>
         <input type="text" name="description" placeholder="description" required>
-        <button type="submit">Ajouter</button>
+        <button class="button"  type="submit">Ajouter</button>
 
     </form>
     <!-- Liste des tâches ne pas oublier les formulaires pour soumettre supprimer etc... nos données une tâche en front-end -->
@@ -81,33 +81,33 @@ $taskss = trouver_tâche_par_userid($_SESSION['user_id']); // c'est ça le read
             <tr>
                 <th>Tâche à faire</th>
                 <th>Description</th>
-                 <th>Actions</th>
+                <th>Actions</th>
             </tr>
             <?php foreach ($taskss as $taches): ?>
                 <tr>
                     <td><?= $taches['name'] ?> </td>
                     <td><?= $taches['description'] ?> </td>
+                    <td>
+                        <!-- Formulaire pour supprimer une tâche en front-end   -->
+                        <form action="" method="POST" style="display: inline;">
+                            <input type="hidden" name="id" value="<?= $taches['id'] ?>">
+                            <button class="button"  type="submit" value="<?= $taches['id'] ?>">Supprimer</button>
+                        </form>
+                        <!-- Formulaire pour modifier (update mise à jour) une tâche en front-end -->
+
+                        <form action="" method="POST">
+                            <input type="hidden" name="id" value="<?= $taches['id'] ?>" required>
+                            <input type="text" name="name" value="<?= $taches['name'] ?>" required>
+                            <input type="text" name="description" value="<?= $taches['description'] ?>" required>
+                            <button class="button"  type="submit">Modifier</button>
+
+                        </form>
+                    </td>
                 </tr>
+                 <?php endforeach; ?>
         </table>
 
-        <!-- read ci dessus -->
-        <!-- Formulaire pour supprimer une tâche en front-end   -->
-
-        <form action="" method="POST" style="display: inline;">
-            <input type="hidden" name="id" value="<?= $taches['id'] ?>">
-            <button type="submit" value="<?= $taches['id'] ?>">Supprimer</button>
-        </form>
-        <!-- Formulaire pour modifier (update mise à jour) une tâche en front-end -->
-
-        <form action="" method="POST">
-            <input type="hidden" name="id" value="<?= $taches['id'] ?>" required>
-            <input type="text" name="name" value="<?= $taches['name'] ?>" required>
-            <input type="text" name="description" value="<?= $taches['description'] ?>" required>
-            <button type="submit">Modifier</button>
-
-        </form>
-
-    <?php endforeach; ?>
+      
 
     </ul>
 
